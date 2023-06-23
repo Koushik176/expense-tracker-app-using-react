1d6533b825a5dafbfdceac006e9cf92e6dbb32ee
Koushik <koushikashok176@gmail.com>
2023-06-23 12:29:27 +0530

    Adding Event Listners

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 
-const ExpenseItem = (props) =>  {
+const ExpenseItem = (props) => {
+  const clickHandler1 = () => {
+    console.log("Clicked!!");
+  };
+  const clickHandler2 = () => {
+    console.log('deleted!!');
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -12,8 +19,12 @@ const ExpenseItem = (props) =>  {
         amount={props.amount}
         locationOfItem={props.locationOfItem}
       />
+      <div>
+      <button onClick={clickHandler1}>Change Title</button>
+      <button onClick={clickHandler2}>Delete</button>
+      </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
