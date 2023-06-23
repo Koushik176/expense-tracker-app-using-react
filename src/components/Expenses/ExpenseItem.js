7a401bf516ae303cc16c7ccdb5d81f5e337d670f
Koushik <koushikashok176@gmail.com>
2023-06-23 14:30:48 +0530

    useState function to change value

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,39 @@
+import React, { useState } from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+
+
   const clickHandler1 = () => {
-    console.log("Clicked!!");
+    setTitle("Updated!");
+    console.log(title);
   };
   const clickHandler2 = () => {
     console.log('deleted!!');
   }
 
+  const clickHandler3 = () => {
+    setAmount(100);
+    console.log(amount);
+  }
+
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
-        title={props.title}
-        amount={props.amount}
+        title={title}
+        amount={amount}
         locationOfItem={props.locationOfItem}
       />
-      <div>
       <button onClick={clickHandler1}>Change Title</button>
       <button onClick={clickHandler2}>Delete</button>
-      </div>
+      <button onClick={clickHandler3}>Change Amount</button>
     </Card>
   );
 };
