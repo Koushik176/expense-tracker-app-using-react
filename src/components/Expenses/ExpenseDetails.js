492f1366a05ce6ff8ae0c4d99dba9a336b368774
Koushik <koushikashok176@gmail.com>
2024-05-17 11:36:34 +0530

    Year alter in Filter

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,13 +1,13 @@
-import './ExpenseDetails.css';
+import "./ExpenseDetails.css";
 
 const ExpenseDetails = (props) => {
   return (
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <div className="expense-item__price">${+props.amount}</div>
-      <div>{props.locationOfItem}</div>
+      <div className="expense-item__location">{props.locationOfItem}</div>
     </div>
   );
-}
+};
 
 export default ExpenseDetails;
